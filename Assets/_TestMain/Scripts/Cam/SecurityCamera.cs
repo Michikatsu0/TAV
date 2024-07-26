fb07cfdb80aa5becead5166ea84cb228711bcd9f
juananre <78058130+juananre@users.noreply.github.com>
2024-07-25 20:04:17 -0500

    configracion

---
+++ Assets/_TestMain/Scripts/Cam/SecurityCamera.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using static UnityEditor.SceneView;
+
+public class SecurityCamera : MonoBehaviour
+{
+    [SerializeField] private CameraSettings settings;
+    [SerializeField] private Transform player;
+    [SerializeField] private Renderer cameraLens;
+    [SerializeField] private Slider detectionSlider;
+
+    private float detectionTimer = 0f;
+    private int alertLevel = 0;
+    private Image fillImage;
+    private Transform cam;
+    private float alertCooldownTimer = 0f;
+
+    void Start()
+    {
+        cam = Camera.main.transform;
+        SetCameraState(settings.patrolColor, 0);
+        detectionSlider.maxValue = settings.detectionTime;
+        detectionSlider.value = 0;
+        fillImage = detectionSlider.fillRect.GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        detectionSlider.transform.rotation = cam.rotation;
+
+        if (alertLevel > 0)
+        {
+            alertCooldownTimer -= Time.deltaTime;
+            if (alertCooldownTimer <= 0)
+            {
+                SetCameraState(settings.patrolColor, 0);
+                detectionTimer = 0f;
+            }
+        }
+
+        DetectPlayer();
+        UpdateDetectionSlider();
+    }
+
+    void DetectPlayer()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if (distanceToPlayer <= settings.detectionRange)
+        {
+            detectionTimer += Time.deltaTime;
+            if (detectionTimer >= settings.detectionTime && alertLevel == 0)
+            {
+                SetCameraState(settings.alertColor, 1);
+            }
+        }
+        else
+        {
+            detectionTimer = Mathf.Max(0, detectionTimer - Time.deltaTime);
+        }
+    }
+
+    void UpdateDetectionSlider()
+    {
+        detectionSlider.value = Mathf.Lerp(detectionSlider.value, detectionTimer, settings.transitionDamageLerp * Time.deltaTime);
+    }
+
+    void SetCameraState(Color lensColor, int level)
+    {
+        cameraLens.material.color = lensColor;
+        alertLevel = level;
+        alertCooldownTimer = settings.alertDuration;
+
+        // Additional logic for different alert levels
+        if (alertLevel == 1)
+        {
+            StartCoroutine(AlertEnemies());
+        }
+        else if (alertLevel == 0)
+        {
+            detectionSlider.value = 0;
+        }
+    }
+
+    IEnumerator AlertEnemies()
+    {
+        // Logic to alert enemies
+        // For example, you can find nearby enemies and set them to patrol the area
+        yield return new WaitForSeconds(1f); // Placeholder delay
+    }
+}
--- Assets/_TestMain/Scripts/Cam/StatsCamEnemy.cs
+++ Assets/_TestMain/Scripts/Cam/StatsCamEnemy.cs
@@ -3,24 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Base Data Cam Stats", order = 1)]
-public class StatsCamEnemy : ScriptableObject, ISerializationCallbackReceiver
+public class CameraSettings : ScriptableObject, ISerializationCallbackReceiver
 {
-    [Header("Health Settings")]
-    [SerializeField] public float maxHealth;
-    [SerializeField] public float deathTime;
+    [Header("Detection Settings")]
+    [SerializeField] public float detectionRange;
+    [SerializeField] public float detectionTime;
+    [Header("Lens Colors")]
+    [SerializeField] public Color patrolColor;
+    [SerializeField] public Color alertColor;
+    [SerializeField] public Color attackColor;
+    [Header("Alert Settings")]
+    [SerializeField] public float alertDuration;
+    [SerializeField] public float transitionDamageLerp;
 
-    // Start is called before the first frame update
-    public void Init()
-    {
+    public void Init() { }
 
-    }
     public void OnBeforeSerialize()
     {
         Init();
     }
 
-    public void OnAfterDeserialize()
-    {
-
-    }
+    public void OnAfterDeserialize() { }
 }
